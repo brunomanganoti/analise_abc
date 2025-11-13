89a594b1c2daba8bb6ebe9e84c049d76f996f0a2
Bruno Manganoti <brunomanganoti@gmail.com>
2025-11-13 09:58:19 -0300

    arquivo de respostas e teste sqldf

--- analise.py
+++ analise.py
@@ -1,4 +1,16 @@
 import pandas as pd
 from pandasql import sqldf
+from matplotlib import pyplot
 
-print('funcionando!')
+df_clientes    = pd.read_csv('data/clientes.csv')
+df_itenspedido = pd.read_csv('data/itens_pedido.csv')
+df_pedidos     = pd.read_csv('data/pedidos.csv')
+df_produtos    = pd.read_csv('data/produtos.csv')
+
+query = """
+SELECT * FROM df_clientes
+WHERE estado = 'SP'
+LIMIT 10
+"""
+
+print(sqldf(query))
